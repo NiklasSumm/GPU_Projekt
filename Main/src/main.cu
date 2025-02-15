b4dd76510095986ceb5704fa6a2f7d9fbd59758c
NiklasSumm <42118513+NiklasSumm@users.noreply.github.com>
2025-02-15 14:48:05 +0100

    Update main.cu

--- Main/src/main.cu
+++ Main/src/main.cu
@@ -40,67 +40,67 @@ template <int blockSize>
 __global__ void
 setupKernel1(int numElements, long *input)
 {
-	//int iterations = (1023 + blockDim.x) / blockDim.x;
-//
-	//unsigned int blockSum = 0;
-	//unsigned int aggregate = 0;
-//
-	//using BlockScan = cub::BlockScan<unsigned int, blockSize>;
-	//__shared__ typename BlockScan::TempStorage temp_storage;
-//
-	//for (int i = 0; i < iterations; i++) {
-	//	int elementId = blockIdx.x * 1024 + i * blockDim.x + threadIdx.x;
-//
-	//	// Load 64 bit bitmask section and count bits
-	//	unsigned int original_data = 0;
-	//	if (elementId < numElements) {
-	//		original_data = __popcll(input[elementId]);
-	//	}
-	//	unsigned int thread_data;
-//
-	//	// Collectively compute the block-wide exclusive sum
-	//	BlockScan(temp_storage).ExclusiveSum(original_data, thread_data, aggregate);
-//
-	//	// First thread of each warp writes in layer 1
-	//	if ((threadIdx.x & 31) == 0) {
-	//		reinterpret_cast<unsigned short*>(input)[numElements*4+elementId/32] = static_cast<unsigned short>(thread_data + blockSum);
-	//		//printf("%i\n", thread_data + blockSum);
-	//	}
-//
-	//	blockSum += aggregate;
-//
-	//	// Last thread of each full block writes into layer 2
-	//	if (threadIdx.x == blockDim.x - 1) {
-	//		int offset = numElements*2 + ((numElements+31)/32 + 1)/2;
-	//		reinterpret_cast<unsigned int*>(input)[offset+blockIdx.x] = blockSum;
-	//		printf("%i\n", blockSum);
-	//	}
-	//}
+	int iterations = (1023 + blockDim.x) / blockDim.x;
 
-	int elementId = blockIdx.x * blockDim.x + threadIdx.x;
+	unsigned int blockSum = 0;
+	unsigned int aggregate = 0;
 
-	if (elementId < numElements) {
-		using BlockScan = cub::BlockScan<unsigned int, 1024>;
-		__shared__ typename BlockScan::TempStorage temp_storage;
+	using BlockScan = cub::BlockScan<unsigned int, blockSize>;
+	__shared__ typename BlockScan::TempStorage temp_storage;
+
+	for (int i = 0; i < iterations; i++) {
+		int elementId = blockIdx.x * 1024 + i * blockDim.x + threadIdx.x;
 
 		// Load 64 bit bitmask section and count bits
-		unsigned int original_data = __popcll(input[elementId]);
+		unsigned int original_data = 0;
+		if (elementId < numElements) {
+			original_data = __popcll(input[elementId]);
+		}
 		unsigned int thread_data;
 
 		// Collectively compute the block-wide exclusive sum
-		BlockScan(temp_storage).ExclusiveSum(original_data, thread_data);
+		BlockScan(temp_storage).ExclusiveSum(original_data, thread_data, aggregate);
 
 		// First thread of each warp writes in layer 1
 		if ((threadIdx.x & 31) == 0) {
-			reinterpret_cast<unsigned short*>(input)[numElements*4+elementId/32] = static_cast<unsigned short>(thread_data);
+			reinterpret_cast<unsigned short*>(input)[numElements*4+elementId/32] = static_cast<unsigned short>(thread_data + blockSum);
+			//printf("%i\n", thread_data + blockSum);
 		}
 
-		// Last thread of each full block writes into layer 2
-		if (threadIdx.x == 1023) {
-			int offset = numElements*2 + ((numElements+31)/32 + 1)/2;
-			reinterpret_cast<unsigned int*>(input)[offset+blockIdx.x] = thread_data + original_data;
-		}
+		blockSum += aggregate;
 	}
+
+	// Last thread of each full block writes into layer 2
+	if (threadIdx.x == blockDim.x - 1) {
+		int offset = numElements*2 + ((numElements+31)/32 + 1)/2;
+		reinterpret_cast<unsigned int*>(input)[offset+blockIdx.x] = blockSum;
+		printf("%i\n", blockSum);
+	}
+
+	//int elementId = blockIdx.x * blockDim.x + threadIdx.x;
+//
+	//if (elementId < numElements) {
+	//	using BlockScan = cub::BlockScan<unsigned int, 1024>;
+	//	__shared__ typename BlockScan::TempStorage temp_storage;
+//
+	//	// Load 64 bit bitmask section and count bits
+	//	unsigned int original_data = __popcll(input[elementId]);
+	//	unsigned int thread_data;
+//
+	//	// Collectively compute the block-wide exclusive sum
+	//	BlockScan(temp_storage).ExclusiveSum(original_data, thread_data);
+//
+	//	// First thread of each warp writes in layer 1
+	//	if ((threadIdx.x & 31) == 0) {
+	//		reinterpret_cast<unsigned short*>(input)[numElements*4+elementId/32] = static_cast<unsigned short>(thread_data);
+	//	}
+//
+	//	// Last thread of each full block writes into layer 2
+	//	if (threadIdx.x == 1023) {
+	//		int offset = numElements*2 + ((numElements+31)/32 + 1)/2;
+	//		reinterpret_cast<unsigned int*>(input)[offset+blockIdx.x] = thread_data + original_data;
+	//	}
+	//}
 
 }
 
