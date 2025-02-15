bb3b640376cef4299f18d010c1e0e0a5f4e5fb45
NiklasSumm <42118513+NiklasSumm@users.noreply.github.com>
2025-02-15 15:50:22 +0100

    Update main.cu

--- Main/src/main.cu
+++ Main/src/main.cu
@@ -483,20 +483,20 @@ int layerSize(int layer, int bitmaskSize) {
 void setup115(int numElements, long *d_bitmask) {
 	setupKernel1<1024><<<(numElements+1023)/1024, 1024>>>(numElements, d_bitmask);
 
-	int offset;
-	if (layerSize(2, numElements) > 0) {
-		printf("running second kernel...\n");
-		offset = layerSize(0, numElements) + layerSize(1, numElements);
-		int size = layerSize(2, numElements);
-		setupKernel2<256><<<(size+1023)/1024, 256>>>(size, &reinterpret_cast<unsigned int*>(d_bitmask)[offset]);
-	}
-
-	if (layerSize(4, numElements) > 0) {
-		printf("running third kernel...\n");
-		offset += layerSize(2, numElements) + layerSize(3, numElements);
-		int size = layerSize(4, numElements);
-		setupKernel2<256><<<(size+1023)/1024, 256>>>(size, &reinterpret_cast<unsigned int*>(d_bitmask)[offset], false, false);
-	}
+	//int offset;
+	//if (layerSize(2, numElements) > 0) {
+	//	printf("running second kernel...\n");
+	//	offset = layerSize(0, numElements) + layerSize(1, numElements);
+	//	int size = layerSize(2, numElements);
+	//	setupKernel2<256><<<(size+1023)/1024, 256>>>(size, &reinterpret_cast<unsigned int*>(d_bitmask)[offset]);
+	//}
+//
+	//if (layerSize(4, numElements) > 0) {
+	//	printf("running third kernel...\n");
+	//	offset += layerSize(2, numElements) + layerSize(3, numElements);
+	//	int size = layerSize(4, numElements);
+	//	setupKernel2<256><<<(size+1023)/1024, 256>>>(size, &reinterpret_cast<unsigned int*>(d_bitmask)[offset], false, false);
+	//}
 }
 
 void setup78(int numElements, long *d_bitmask) {
