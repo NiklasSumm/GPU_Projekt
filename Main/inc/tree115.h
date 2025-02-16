0a4b772eadae0aabda6feb6745060fb20032fea5
NiklasSumm <42118513+NiklasSumm@users.noreply.github.com>
2025-02-16 10:26:08 +0100

    Update tree115.h

--- Main/inc/tree115.h
+++ Main/inc/tree115.h
@@ -130,9 +130,9 @@ improvedApply(int numPacked, int *permutation, int bitmaskSize, TreeStructure st
 					searchIndex = (searchIndex < 0) ? 0 : ((searchIndex < layerSize) ? searchIndex : layerSize - 1);
 					layerSum = static_cast<uint32_t>(layer[searchIndex]);
 				}
-				if (layerSum >= bitsToFind && serachIndex > 0){
+				if (layerSum >= bitsToFind && searchIndex > 0){
 					searchIndex--;
-					layerSum = static_cast<uint32_t>(layer[i]);
+					layerSum = static_cast<uint32_t>(layer[searchIndex]);
 				}
 				bitsToFind -= layerSum;
 				nextLayerOffset += i;
@@ -166,9 +166,9 @@ improvedApply(int numPacked, int *permutation, int bitmaskSize, TreeStructure st
 				searchIndex = (searchIndex < 0) ? 0 : ((searchIndex < layerSize) ? searchIndex : layerSize - 1);
 				layerSum = static_cast<uint32_t>(layer[searchIndex]);
 			}
-			if (layerSum >= bitsToFind && serachIndex > 0){
+			if (layerSum >= bitsToFind && searchIndex > 0){
 				searchIndex--;
-				layerSum = static_cast<uint32_t>(layer[i]);
+				layerSum = static_cast<uint32_t>(layer[searchIndex]);
 			}
 			bitsToFind -= layerSum;
 			nextLayerOffset += i;
