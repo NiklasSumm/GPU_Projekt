bd4c339e7a7be22b850be730a186b0300867bf49
NiklasSumm <42118513+NiklasSumm@users.noreply.github.com>
2025-02-19 16:26:44 +0100

    Update tree115.h

--- Main/inc/tree115.h
+++ Main/inc/tree115.h
@@ -10,7 +10,7 @@ setupKernel1(int numElements, long *input)
 {
 	int iterations = (1023 + blockDim.x) / blockDim.x;
 
-	unsigned int aggregateSum = 0;
+	//unsigned int aggregateSum = 0;
 	unsigned int aggregate = 0;
 
 	using BlockScan = cub::BlockScan<unsigned int, blockSize>;
@@ -28,21 +28,21 @@ setupKernel1(int numElements, long *input)
 		unsigned int thread_data;
 
 		// Collectively compute the block-wide exclusive sum
-		BlockScan(temp_storage).ExclusiveSum(original_data, thread_data, aggregate);
+		BlockScan(temp_storage).ExclusiveScan(original_data, thread_data, aggregate, cuda::plus<>{}, aggregate);
 
 		// First thread of each warp writes in layer 1
 		if (((threadIdx.x & 31) == 0) && (elementId < numElements)) {
-			reinterpret_cast<unsigned short*>(input)[numElements*4+elementId/32] = static_cast<unsigned short>(thread_data + aggregateSum);
+			reinterpret_cast<unsigned short*>(input)[numElements*4+elementId/32] = static_cast<unsigned short>(thread_data);
 		}
 
 		// Accumulate the aggregate for the next iteration of the loop 
-		aggregateSum += aggregate;
+		//aggregateSum += aggregate;
 	}
 
 	// Last thread of each full block writes into layer 2
 	if ((threadIdx.x == blockDim.x - 1) && (elementId < numElements)) {
 		int offset = numElements*2 + ((numElements+31)/32 + 1)/2;
-		reinterpret_cast<unsigned int*>(input)[offset+blockIdx.x] = aggregateSum;
+		reinterpret_cast<unsigned int*>(input)[offset+blockIdx.x] = aggregate;
 	}
 }
 
@@ -199,7 +199,7 @@ improvedApply(int numPacked, int *permutation, int bitmaskSize, TreeStructure st
 			//}
 
 			int searchIndex = 0;
-			int searchStep = cuda::std::bit_ceil(1);
+			int searchStep = nextPowOf2; //cuda::std::bit_ceil<int>(layerSize);
 
 			while (searchStep > 1){
 				searchStep >>= 1;
