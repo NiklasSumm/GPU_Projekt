8af13a8dc7b3b640a1b8b098d8988f1db357ee56
NiklasSumm <42118513+NiklasSumm@users.noreply.github.com>
2025-02-24 22:56:48 +0100

    Update tree78.h

--- Main/inc/tree78.h
+++ Main/inc/tree78.h
@@ -8,11 +8,11 @@ struct BlockPrefixCallbackOp
     int running_total;
 
     // Constructor
-    __host__ BlockPrefixCallbackOp(int running_total) : running_total(running_total) {}
+    __device__ BlockPrefixCallbackOp(int running_total) : running_total(running_total) {}
 
     // Callback operator to be entered by the first warp of threads in the block.
     // Thread-0 is responsible for returning a value for seeding the block-wide scan.
-    __host__ int operator()(int block_aggregate)
+    __device__ int operator()(int block_aggregate)
     {
         int old_prefix = running_total;
         running_total += block_aggregate;
