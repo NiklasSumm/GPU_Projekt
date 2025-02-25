969caab92624f4e12c40233cc7277c45b5f78462
NiklasSumm <42118513+NiklasSumm@users.noreply.github.com>
2025-02-25 15:00:51 +0100

    Update encodingBase.h

--- Main/inc/encodingBase.h
+++ Main/inc/encodingBase.h
@@ -17,19 +17,6 @@ class TreeStructure {
         int layerSizes[5];
 };
 
-__device__ int getNextLargestPowerOf2(int num){
-    if (num & (num - 1)){
-		num |= num >> 1;
-    	num |= num >> 2;
-    	num |= num >> 4;
-    	num |= num >> 8;
-    	num |= num >> 16;
-
-		num = (num ^ (num << 1)) - 1;
-	}
-    return num;
-}
-
 #define UNUSED(x) (void)(x) // Disables compiler warnings
 
 #endif
