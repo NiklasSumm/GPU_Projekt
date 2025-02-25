6ed7b7d2fa27524c25a1dd054dd654fb84644081
NiklasSumm <42118513+NiklasSumm@users.noreply.github.com>
2025-02-25 09:40:12 +0100

    Update tree88.h

--- Main/inc/tree88.h
+++ Main/inc/tree88.h
@@ -1,68 +1,72 @@
 #include <cub/cub.cuh>
-
+#include <block_prefix_callback_op.h>
 #include <encodingBase.h>
 
 template <int blockSize, int layer1Size, int layer2Size>
 __global__ void
-setupKernel88(int numElements, uint64_t *input)
+setupKernelFixedExclusive(int numElements, uint64_t *input)
 {
 	int iterations = (((1 << (layer1Size - 6)) * (1 << layer2Size)) + blockDim.x - 1) / blockDim.x;
 
-	unsigned int aggregateSum = 0;
-	unsigned int aggregate = 0;
+    //unsigned int aggregateSum = 0;
+    //unsigned int aggregate = 0;
 
-	using BlockScan = cub::BlockScan<unsigned int, blockSize>;
-	__shared__ typename BlockScan::TempStorage temp_storage;
+    using BlockScan = cub::BlockScan<unsigned int, blockSize>;
+    __shared__ typename BlockScan::TempStorage temp_storage;
 
-	unsigned int elementId = 0;
+    BlockPrefixCallbackOp prefix_op(0);
+
+    unsigned int elementId = 0;
+    unsigned int original_data = 0;
+    unsigned int thread_data;
 
 	for (int i = 0; i < iterations; i++) {
 		elementId = blockIdx.x * ((1 << (layer1Size - 6)) * (1 << layer2Size)) + i * blockDim.x + threadIdx.x;
 
-		// Load 64 bit bitmask section and count bits
-		unsigned int original_data = 0;
-		if (elementId < numElements) 
-			original_data = __popcll(input[elementId]);
-		unsigned int thread_data;
+        // Load 64 bit bitmask section and count bits
+        if (elementId < numElements)
+            original_data = __popcll(input[elementId]);
+        else
+            original_data = 0;
 
-		// Collectively compute the block-wide inclusive sum
-		BlockScan(temp_storage).ExclusiveSum(original_data, thread_data, aggregate);
+        // Collectively compute the block-wide inclusive sum
+        BlockScan(temp_storage).ExclusiveSum(original_data, thread_data, prefix_op);
 
 		// Every fourth thread writes value in first layer
 		if (((threadIdx.x & ((1 << (layer1Size - 6)) - 1)) == 0) && (elementId < numElements)) {
-			reinterpret_cast<unsigned short*>(input)[numElements*4+elementId/((1 << (layer1Size - 6)))] = static_cast<unsigned short>(thread_data + aggregateSum);
+			reinterpret_cast<unsigned short*>(input)[numElements*4+elementId/((1 << (layer1Size - 6)))] = static_cast<unsigned short>(thread_data);
 		}
 
-		// Accumulate the aggregate for the next iteration of the loop 
-		aggregateSum += aggregate;
-	}
+        // Accumulate the aggregate for the next iteration of the loop
+        //aggregateSum += aggregate;
+    }
 
 	// Last thread of each full block writes into layer 2
 	if ((threadIdx.x == blockDim.x - 1) && (elementId < numElements)) {
 		int offset = numElements*2 + ((numElements+(1 << (layer1Size - 6))-1)/(1 << (layer1Size - 6)) + 1)/2;
-		reinterpret_cast<unsigned int*>(input)[offset+blockIdx.x] = aggregateSum;
+		reinterpret_cast<unsigned int*>(input)[offset+blockIdx.x] = thread_data + original_data;
 	}
 }
 
 template <int layer1Size, int layer2Size>
 __global__ void
-apply88(int numPacked, int *dst, int *src, int bitmaskSize, TreeStructure structure, bool unpack)
+applyFixedExclusive(int numPacked, int *dst, int *src, int bitmaskSize, TreeStructure structure, bool unpack)
 {
-	int elementIdx = blockIdx.x * blockDim.x + threadIdx.x;
+    int elementIdx = blockIdx.x * blockDim.x + threadIdx.x;
 
-	if (elementIdx < numPacked) {
-		uint32_t bitsToFind = elementIdx+1;
+    if (elementIdx < numPacked) {
+        uint32_t bitsToFind = elementIdx+1;
 
-		int nextLayerOffset = 0;
-		int layerSize = structure.layerSizes[2];
-		if (layerSize > 1) {
-			uint32_t *layer2 = &structure.layers[2][0];
+        int nextLayerOffset = 0;
+        int layerSize = structure.layerSizes[2];
+        if (layerSize > 1) {
+            uint32_t *layer2 = &structure.layers[2][0];
 
-			// Index and step for binary search
-			int searchIndex = layerSize / 2;
-			int searchStep = (layerSize + 1) / 2;
+            // Index and step for binary search
+            int searchIndex = layerSize / 2;
+            int searchStep = (layerSize + 1) / 2;
 
-			uint32_t layerSum = static_cast<uint32_t>(layer2[searchIndex]);
+            uint32_t layerSum = static_cast<uint32_t>(layer2[searchIndex]);
 
 			while (searchStep > 1){
 				searchStep = (searchStep + 1) / 2;
@@ -83,18 +87,18 @@ apply88(int numPacked, int *dst, int *src, int bitmaskSize, TreeStructure struct
 			nextLayerOffset *= (1 << layer2Size);
 		}
 
-		// Handle layer 1
-		layerSize = (structure.layerSizes[1] + structure.layerSizes[2] - 1) / structure.layerSizes[2];
-		
-		if (layerSize > 1) {
-			layerSize = min(layerSize, structure.layerSizes[1] - nextLayerOffset);
-			uint16_t *layer1 = &reinterpret_cast<uint16_t *>(structure.layers[1])[nextLayerOffset];
+        // Handle layer 1
+        layerSize = (structure.layerSizes[1] + structure.layerSizes[2] - 1) / structure.layerSizes[2];
 
-			// Index and step for binary search
-			int searchIndex = layerSize / 2;
-			int searchStep = (layerSize + 1) / 2;
+        if (layerSize > 1) {
+            layerSize = min(layerSize, structure.layerSizes[1] - nextLayerOffset);
+            uint16_t *layer1 = &reinterpret_cast<uint16_t *>(structure.layers[1])[nextLayerOffset];
 
-			uint32_t layerSum = static_cast<uint32_t>(layer1[searchIndex]);
+            // Index and step for binary search
+            int searchIndex = layerSize / 2;
+            int searchStep = (layerSize + 1) / 2;
+
+            uint32_t layerSum = static_cast<uint32_t>(layer1[searchIndex]);
 
 			while (searchStep > 1){
 				searchStep = (searchStep + 1) / 2;
@@ -115,47 +119,47 @@ apply88(int numPacked, int *dst, int *src, int bitmaskSize, TreeStructure struct
 			nextLayerOffset *= (1 << (layer1Size - 6));;
 		}
 
-		// Handle virtual layer 0 (before bitmask)
-		uint64_t bitmaskSection;
-		layerSize = structure.layerSizes[0]/2 - nextLayerOffset;
-		//layerSize = min(layerSize, 32);
-		uint64_t *bitLayer = &reinterpret_cast<uint64_t *>(structure.layers[0])[nextLayerOffset];
-		for (int i = 0; i < layerSize; i++) {
-			bitmaskSection = bitLayer[i];
-			int sectionSum = __popcll(bitmaskSection);
-			if (bitsToFind <= sectionSum) break;
-			bitsToFind -= sectionSum;
-			nextLayerOffset++;
-		}
+        // Handle virtual layer 0 (before bitmask)
+        uint64_t bitmaskSection;
+        layerSize = structure.layerSizes[0]/2 - nextLayerOffset;
+        //layerSize = min(layerSize, 32);
+        uint64_t *bitLayer = &reinterpret_cast<uint64_t *>(structure.layers[0])[nextLayerOffset];
+        for (int i = 0; i < layerSize; i++) {
+            bitmaskSection = bitLayer[i];
+            int sectionSum = __popcll(bitmaskSection);
+            if (bitsToFind <= sectionSum) break;
+            bitsToFind -= sectionSum;
+            nextLayerOffset++;
+        }
 
-		// Handle bitmask
-		int expandedIndex = nextLayerOffset * 64;
-		for (int k = sizeof(long)*8-1; k >= 0; k--) {
-			if ((bitmaskSection >> k) & 1) {
-				if (--bitsToFind == 0) {
-					expandedIndex += ((sizeof(long)*8-1)-k);
-					break;
-				}
-			}
-		}
-		
+        // Handle bitmask
+        int expandedIndex = nextLayerOffset * 64;
+        for (int k = sizeof(long)*8-1; k >= 0; k--) {
+            if ((bitmaskSection >> k) & 1) {
+                if (--bitsToFind == 0) {
+                    expandedIndex += ((sizeof(long)*8-1)-k);
+                    break;
+                }
+            }
+        }
+
         if (src) {
-			if (unpack) {
-				dst[expandedIndex] = src[elementIdx]; // Unpack: Load from packed, write to expanded
-			} else {
-				dst[elementIdx] = src[expandedIndex]; // Pack: Load from expanded, write to packed
-			}
-		} else {
-			dst[elementIdx] = expandedIndex; // Write permutation (used in apply)
-		}
-	}
+            if (unpack) {
+                dst[expandedIndex] = src[elementIdx]; // Unpack: Load from packed, write to expanded
+            } else {
+                dst[elementIdx] = src[expandedIndex]; // Pack: Load from expanded, write to packed
+            }
+        } else {
+            dst[elementIdx] = expandedIndex; // Write permutation (used in apply)
+        }
+    }
 }
 
 // layerSize calculates the amount of elements per layer, regardless of the actual size on disk.
 // For the bitmask, we return the amount of integers.
 template <int layer1Size, int layer2Size>
-int layerSize88(int layer, int bitmaskSize) {
-	int size = bitmaskSize * 2; // Bitmask size is size in long
+int layerSizeFixedExclusive(int layer, int bitmaskSize) {
+    int size = bitmaskSize * 2; // Bitmask size is size in long
 
 	if (layer == 1){
 		size = (bitmaskSize+ (1 << (layer1Size - 6)) - 1) / (1 << (layer1Size - 6));
@@ -164,24 +168,24 @@ int layerSize88(int layer, int bitmaskSize) {
 		size = (bitmaskSize+((1 << (layer1Size - 6)) * (1 << layer2Size) - 1)) / ((1 << (layer1Size - 6)) * (1 << layer2Size));
 	}
 
-	return size;
+    return size;
 }
 
 template <int layer1Size, int layer2Size>
-int layerOffsetInt88(int layer, int bitmaskSize) {
-	if (layer == 0) return 0;
+int layerOffsetIntFixedExclusive(int layer, int bitmaskSize) {
+    if (layer == 0) return 0;
 
-	int offset = 0;
-	for (int i = 0; i < layer; i++) {
-		int size = layerSize88<layer1Size, layer2Size>(i, bitmaskSize);
-		if (i == 1) size = (size+1)/2;
-		offset += size;
-	}
-	return offset;
+    int offset = 0;
+    for (int i = 0; i < layer; i++) {
+        int size = layerSizeFixedExclusive<layer1Size, layer2Size>(i, bitmaskSize);
+        if (i == 1) size = (size+1)/2;
+        offset += size;
+    }
+    return offset;
 }
 
 template <int blockSize, int layer1Size, int layer2Size>
-class Tree88 : public EncodingBase {
+class FixedExclusive : public EncodingBase {
 	private:
 		uint64_t *d_bitmask;
 		int n;
@@ -190,7 +194,7 @@ class Tree88 : public EncodingBase {
 		void setup(uint64_t *d_bitmask, int n) {
             int gridSize = (n + ((1 << (layer1Size - 6)) * (1 << layer2Size)) - 1) / ((1 << (layer1Size - 6)) * (1 << layer2Size));
 
-			setupKernel88<blockSize,layer1Size,layer2Size><<<gridSize, blockSize>>>(n, d_bitmask);
+            setupKernelFixedExclusive<blockSize,layer1Size,layer2Size><<<gridSize, blockSize>>>(n, d_bitmask);
 
 	        int offset = n*2 + ((n+(1 << (layer1Size - 6))-1)/(1 << (layer1Size - 6)) + 1)/2;
             int size = gridSize;
@@ -207,48 +211,48 @@ class Tree88 : public EncodingBase {
             // Run exclusive prefix sum
             cub::DeviceScan::ExclusiveSum(d_temp_storage, temp_storage_bytes, startPtr, startPtr, size);
 
-			this->d_bitmask = d_bitmask;
-			this->n = n;
-		};
+            this->d_bitmask = d_bitmask;
+            this->n = n;
+        };
 
-		void apply(int *permutation, int packedSize) {
-			TreeStructure ts;
+        void apply(int *permutation, int packedSize) {
+            TreeStructure ts;
 
-			uint32_t *d_bitmask_int = reinterpret_cast<uint32_t*>(d_bitmask);
-			for (int layer = 0; layer < 3; layer++) {
-				ts.layers[layer] = &d_bitmask_int[layerOffsetInt88<layer1Size,layer2Size>(layer, n)];
-				ts.layerSizes[layer] = layerSize88<layer1Size,layer2Size>(layer, n);
-			}
+            uint32_t *d_bitmask_int = reinterpret_cast<uint32_t*>(d_bitmask);
+            for (int layer = 0; layer < 3; layer++) {
+                ts.layers[layer] = &d_bitmask_int[layerOffsetIntFixedExclusive<layer1Size,layer2Size>(layer, n)];
+                ts.layerSizes[layer] = layerSizeFixedExclusive<layer1Size,layer2Size>(layer, n);
+            }
 
-            apply88<layer1Size, layer2Size><<<(packedSize+127)/128, 128>>>(packedSize, permutation, nullptr, n, ts, false);
-		};
+            applyFixedExclusive<layer1Size, layer2Size><<<(packedSize+127)/128, 128>>>(packedSize, permutation, nullptr, n, ts, false);
+        };
 
         void pack(int *src, int *dst, int packedSize) {
             TreeStructure ts;
 
-			uint32_t *d_bitmask_int = reinterpret_cast<uint32_t*>(d_bitmask);
-			for (int layer = 0; layer < 3; layer++) {
-				ts.layers[layer] = &d_bitmask_int[layerOffsetInt88<layer1Size, layer2Size>(layer, n)];
-				ts.layerSizes[layer] = layerSize88<layer1Size, layer2Size>(layer, n);
-			}
+            uint32_t *d_bitmask_int = reinterpret_cast<uint32_t*>(d_bitmask);
+            for (int layer = 0; layer < 3; layer++) {
+                ts.layers[layer] = &d_bitmask_int[layerOffsetIntFixedExclusive<layer1Size, layer2Size>(layer, n)];
+                ts.layerSizes[layer] = layerSizeFixedExclusive<layer1Size, layer2Size>(layer, n);
+            }
 
-			apply88<layer1Size, layer2Size><<<(packedSize+127)/128, 128>>>(packedSize, dst, src, n, ts, false);
+            applyFixedExclusive<layer1Size, layer2Size><<<(packedSize+127)/128, 128>>>(packedSize, dst, src, n, ts, false);
         };
 
         void unpack(int *src, int *dst, int packedSize) {
             TreeStructure ts;
 
-			uint32_t *d_bitmask_int = reinterpret_cast<uint32_t*>(d_bitmask);
-			for (int layer = 0; layer < 3; layer++) {
-				ts.layers[layer] = &d_bitmask_int[layerOffsetInt88<layer1Size, layer2Size>(layer, n)];
-				ts.layerSizes[layer] = layerSize88<layer1Size, layer2Size>(layer, n);
-			}
+            uint32_t *d_bitmask_int = reinterpret_cast<uint32_t*>(d_bitmask);
+            for (int layer = 0; layer < 3; layer++) {
+                ts.layers[layer] = &d_bitmask_int[layerOffsetIntFixedExclusive<layer1Size, layer2Size>(layer, n)];
+                ts.layerSizes[layer] = layerSizeFixedExclusive<layer1Size, layer2Size>(layer, n);
+            }
 
-			apply88<layer1Size, layer2Size><<<(packedSize+127)/128, 128>>>(packedSize, dst, src, n, ts, true);
+            applyFixedExclusive<layer1Size, layer2Size><<<(packedSize+127)/128, 128>>>(packedSize, dst, src, n, ts, true);
         };
-	
-		void print(uint64_t *h_bitmask) {
-			// Print bitmask
+
+        void print(uint64_t *h_bitmask) {
+            // Print bitmask
             if (n < 100) {
                 std::cout << "bitmask: ";
                 for (int i = 0; i < n; i++) {
@@ -281,5 +285,5 @@ class Tree88 : public EncodingBase {
                 }
                 std::cout << std::endl;
             }
-		};
+        };
 };
