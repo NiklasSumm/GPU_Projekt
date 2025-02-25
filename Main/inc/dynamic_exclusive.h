6fb10e003f49f1ca61521142a1aa5bfc85043136
NiklasSumm <42118513+NiklasSumm@users.noreply.github.com>
2025-02-25 14:43:00 +0100

    Update dynamic_exclusive.h

--- Main/inc/dynamic_exclusive.h
+++ Main/inc/dynamic_exclusive.h
@@ -417,19 +417,6 @@ int layerOffsetInt(int layer, int bitmaskSize) {
     return offset;
 }
 
-int getNextLargestPowerOf2(int num){
-    if (num & (num - 1)){
-		num |= num >> 1;
-    	num |= num >> 2;
-    	num |= num >> 4;
-    	num |= num >> 8;
-    	num |= num >> 16;
-
-		num = (num ^ (num << 1)) - 1;
-	}
-    return num;
-}
-
 template <int blockSize>
 class DynamicExclusive : public EncodingBase {
     private:
