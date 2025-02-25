b47bfcb433504d2e2e9f542bbf870533e0d54332
NiklasSumm <42118513+NiklasSumm@users.noreply.github.com>
2025-02-25 19:01:42 +0100

    Update benchmark.sh

--- Main/benchmark.sh
+++ Main/benchmark.sh
@@ -11,7 +11,7 @@ echo "bitmask_size_bytes, time_setup_115_ms, time_apply_115_ms, time_total_115_m
 echo "bitmask_size_bytes, bw_setup_115, bw_apply_115, bw_total_115, bw_setup_78, bw_apply_78, bw_total_78, bw_setup_88, bw_apply_88, bw_total_88, bw_setup_baseline, bw_apply_baseline, bw_total_baseline, bw_setup_baseline_sl, bw_apply_baseline_sl, bw_total_baseline_sl" > measurement_bandwidth.csv
 
 for size in $size_options; do
-    
+
     echo -n "$size" >> measurement_times.csv
     echo -n "$size" >> measurement_bandwidth.csv
     for block_size in $block_sizes; do
@@ -21,7 +21,7 @@ for size in $size_options; do
         time_setup_ms=`echo "$output" | awk '/Setup Time:/ {print $3}'`
         time_apply_ms=`echo "$output" | awk '/Apply Time:/ {print $3}'`
         time_total_ms=`echo "$output" | awk '/Total Time:/ {print $3}'`
-
+        
         echo -n ", $time_setup_ms, $time_apply_ms, $time_total_ms" >> measurement_times.csv
 
         bw_setup=`echo "$output" | awk '/Setup Bandwidth:/ {print $3}'`
