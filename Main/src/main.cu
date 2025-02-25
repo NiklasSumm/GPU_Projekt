ffaa39d7610cfaf016b7c7d536bb09b3830e3687
NiklasSumm <42118513+NiklasSumm@users.noreply.github.com>
2025-02-25 10:38:53 +0100

    Update main.cu

--- Main/src/main.cu
+++ Main/src/main.cu
@@ -147,7 +147,7 @@ int main(int argc, char *argv[])
     DynamicExclusive<1024> dynamicExclusive = DynamicExclusive<1024>();
     DynamicExclusive<1024> dynamicExclusiveSolo = DynamicExclusive<1024>(false);
     FixedInclusive<512,7,8> fixedInclusive = FixedInclusive<512,7,8>{};
-    FixedExclusive<1024,8,6> fixedExclusive = FixedExclusive<1024,8,6>{};
+    FixedExclusive<1024,6,5> fixedExclusive = FixedExclusive<1024,6,5>{};
     ThrustBaseline baseline = ThrustBaseline(packedSize);
     ThrustBaseline baselineSetupLess = ThrustBaseline();
 
