43c14d29586ec4398717f0f2471bd9fda21047ca
NiklasSumm <42118513+NiklasSumm@users.noreply.github.com>
2025-03-09 17:23:56 +0100

    Update main.cu

--- Main/src/main.cu
+++ Main/src/main.cu
@@ -57,8 +57,8 @@ populateBitmask(int numElements, uint32_t *bitmask, float sparsity, int groupSiz
     if (elementIdx < numElements) {
 
         curandState state;
-        int seed = elementIdx / (groupSize + 31 / 32);
-        curand_init(seed, 0, 0, &state);
+        int offset = (elementIdx / (groupSize / 32)) * groupSize;
+        curand_init(1234, 0, offset, &state);
 
         uint32_t element = 0xffffffff;
         for (uint32_t k = 0; k < sizeof(element) * 8; k += groupSize) {
